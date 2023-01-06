23b19b84e8af9b2779a2520dd3eb64f5b1c051aa
Brayan Andres Hc <118775234+Brayan-Hc11@users.noreply.github.com>
2023-01-06 13:54:10 -0500

    Add files via upload

---
+++ convenciones.js
@@ -0,0 +1,34 @@
+var color = "azul";
+console.log(color);
+
+var color ="rojo";
+console.log(color); 
+
+
+var _convenciones1="UPPER CAMEL CASE"; 
+
+let JavaScript;
+let NumeroUno;
+let EdadUsuario;
+let NumeroTelefono;
+
+var _convenciones2="LOWER CAMEL CASE";
+
+let javaScript;
+let numeroUno;
+let edadUsuario;
+let numeroTelefono;
+
+var _convenciones3 ="PASCAL CASE ";
+
+let JavaScript1;
+let NumeroUno1;
+let EdadUsuario1;
+let NumeroTelefono1;
+
+var _convenciones4 ="SNAKE CASE ";
+
+let Java_script;
+let Numero_uno;
+let Edad_usuario;
+let Numero_telefono;
