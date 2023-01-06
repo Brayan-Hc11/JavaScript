8fb19c94867b98aa4f6fb4e65537bc2fff2c41ed
Brayan Andres Hc <118775234+Brayan-Hc11@users.noreply.github.com>
2023-01-06 13:54:28 -0500

    Add files via upload

---
+++ identificadores.js
@@ -0,0 +1,8 @@
+var _numero1= 29;
+alert( _numero1);
+
+let $texto ="hola";
+alert($texto)
+
+var _identificadores = "solo se puedn usar dos tipos de identificadores en js"
+alert(_identificadores);
