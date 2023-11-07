c538357e580e36323fa3bb08ae04f305d230df59
Brayan Andres Hc <118775234+Brayan-Hc11@users.noreply.github.com>
2023-11-07 13:31:36 -0500

    Add files via upload

---
+++ JavaScript/02-variables/Js/01-App.js
@@ -0,0 +1,41 @@
+/*
+camelCase
+snake_case
+pascalCase
+*/
+
+//Inicializar una variable con un valor
+var producto = "Monitor de 24 pulgadas"
+console.log(Producto);
+
+var producto = "Monitor de 14 pulgadas"
+console.log(Producto);
+
+//js es un lenguaje de tipo dinamico no se especifica tipo dato
+producto = 20;
+console.log(Producto);
+
+
+var disponible;
+disponible = true;
+
+//se esperaba una declaración de variables
+var categoria = 'Computadora',
+    marca = 'Marca famosa',
+    calificación = 5;
+
+// las variables no pueden inciar con numeros 
+/*
+var 99dias; no funciona
+var dias99; funciona
+var 01_; no funciona
+var _01_; funciona
+var _usuarios; funciona
+var __usuarios; funciona
+
+var nombreproducto; funciona
+var nombreproducto; funciona
+var nombre_producto_categotia_precio; funciona
+var NombreProducto; funciona
+var nombre producto; funciona
+*/
